c7679beb105d4b3e590f78d5291ff3b51d76bcd4
ElTinmar <martin.privat@hotmail.fr>
2023-11-07 11:12:25 +0100

    update tests

--- tests/compare_speed.py
+++ tests/compare_speed.py
@@ -7,14 +7,9 @@ import pandas as pd
 import seaborn as sns
 from tqdm import tqdm
 import matplotlib.pyplot as plt
-
-# TODO loop over array size 
-# TODO add a long single-threaded computation (maybe mode ?) 
-
-SZ = (2048,2048) # use a size of (1024,1024) to measure throughput in MB/s
-BIGARRAY = np.random.randint(0, 255, SZ, dtype=np.uint8)
-
 from monitored_ipc import MonitoredIPC, MonitoredQueue, MonitoredRingBuffer, MonitoredZMQ_PushPull, MonitoredArrayQueue
+from scipy.stats import mode
+from timeit import timeit
 
 def consumer(buf: MonitoredIPC, processing_fun: Callable, stop: Event, timeout: float):
     buf.initialize_receiver()
@@ -34,13 +29,13 @@ def do_nothing(array: NDArray) -> None:
 def average(array: NDArray) -> None:
     mu = np.mean(array)
 
-def long_computation(array: NDArray) -> None:
-    # long computation already largely multithreaded 
-    U,S,V = np.linalg.svd(array[0:128,0:128])
+def long_computation_mt(array: NDArray) -> None:
+    # long multithreaded computation  
+    U,S,V = np.linalg.svd(array[0:256,0:256])
 
 def long_computation_st(array: NDArray) -> None:
-    # TODO long single-threaded computation
-    pass
+    # long single-threaded computation
+    mode(array[0:256,0:256], keepdims=False)
 
 def run(
         buffer: MonitoredIPC, 
@@ -78,35 +73,43 @@ def run(
 
 if __name__ == '__main__':
 
-    timing_data = pd.DataFrame(columns=['pfun','shm','ncons','fps_in','fps_out'])
-    max_size_MB = int(1000*np.prod(SZ)/(1024**2))
     nprod = 1 # zmq direct push/pull and array queue support only one producer
-    reps = 10
-    
-    for ncons in range(1,10):
-        for pfun in [do_nothing, average, long_computation]:
+    reps = 5
+    timing_data = pd.DataFrame(columns=['pfun','shm','ncons','fps_in','fps_out', 'frame_sz'])
 
-            print(f'{ncons} cons: {pfun.__name__}')
-            
-            for rep in tqdm(range(reps)):
+    for SZ in [(256,256),(512,512),(1024,1024),(2048,2048)]:
 
-                buffers = {
-                    'Ring buffer':  MonitoredRingBuffer(
-                            num_items = 100, 
-                            item_shape = SZ,
-                            data_type = np.uint8
-                        ),
-                    'Array Queue':  MonitoredArrayQueue(max_mbytes=max_size_MB),
-                    'ZMQ':  MonitoredZMQ_PushPull(
-                            item_shape = SZ,
-                            data_type = np.uint8,
-                            port = 5557
-                        ),
-                    'Queue': MonitoredQueue()
-                }
+        BIGARRAY = np.random.randint(0, 255, SZ, dtype=np.uint8)
+        max_size_MB = int(2000*np.prod(SZ)/(1024**2))
 
-                for name, buf in buffers.items():
-                        
+        # check execution time of processing functions
+        for pfun in [do_nothing, average, long_computation_st, long_computation_mt]:
+            print(f'{pfun.__name__} : {timeit(lambda: pfun(BIGARRAY), number=10)} s')
+
+        for ncons in range(1,10):
+            for pfun in [do_nothing, average, long_computation_mt]:
+
+                print(f'{ncons} cons: {pfun.__name__}')
+                
+                for rep in tqdm(range(reps)):
+
+                    buffers = {
+                        'Ring buffer':  MonitoredRingBuffer(
+                                num_items = 100, 
+                                item_shape = SZ,
+                                data_type = np.uint8
+                            ),
+                        'Array Queue':  MonitoredArrayQueue(max_mbytes=max_size_MB),
+                        'ZMQ':  MonitoredZMQ_PushPull(
+                                item_shape = SZ,
+                                data_type = np.uint8,
+                                port = 5557
+                            ),
+                        'Queue': MonitoredQueue()
+                    }
+
+                    for name, buf in buffers.items():
+                            
                         print(name + 40*'-')
 
                         fps_in, fps_out = run(
@@ -122,13 +125,14 @@ if __name__ == '__main__':
                             'shm': [name],
                             'ncons': [ncons],
                             'fps_in': [fps_in],
-                            'fps_out': [fps_out]
+                            'fps_out': [fps_out],
+                            'frame_sz': [int(np.prod(SZ))]
                         })
                         timing_data = pd.concat([timing_data, row], ignore_index=True)
 
     fig, ax = plt.subplots(1,3)
     sns.lineplot(timing_data[timing_data.pfun == 'do_nothing'], x="ncons", y="fps_out", hue="shm", ax = ax[0])
     sns.lineplot(timing_data[timing_data.pfun == 'average'], x="ncons", y="fps_out", hue="shm", ax = ax[1])
-    sns.lineplot(timing_data[timing_data.pfun == 'long_computation'], x="ncons", y="fps_out", hue="shm", ax = ax[2])
+    sns.lineplot(timing_data[timing_data.pfun == 'long_computation_mt'], x="ncons", y="fps_out", hue="shm", ax = ax[2])
     plt.show()
     
