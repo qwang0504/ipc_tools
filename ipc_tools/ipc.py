aade97ed5b29a81301be6827b42d253da22f6264
ElTinmar <martin.privat@hotmail.fr>
2023-11-08 15:50:53 +0100

    add file

---
+++ ipc_tools/ipc.py
@@ -0,0 +1,67 @@
+from abc import ABC, abstractmethod
+from typing import Optional, Any
+
+class IPC(ABC):
+    '''
+    Multiprocessing Queue-like interface for various IPC methods
+    '''
+
+    # Queue-like methods --------------------------------------------------------------
+
+    @abstractmethod
+    def qsize(self) -> int:
+        pass
+
+    @abstractmethod
+    def empty(self) -> bool:
+        pass
+    
+    @abstractmethod 
+    def full(self) -> bool:
+        pass
+
+    @abstractmethod
+    def put(self, obj: Any, block: Optional[bool] = True, timeout: Optional[float] = None) -> None:
+        '''
+        Raises queue.Full exception if block is True and timeout 
+        '''
+        pass
+    
+    def put_nowait(self, obj: Any) -> None:
+        self.put(obj, False)
+
+    @abstractmethod
+    def get(self, block: Optional[bool] = True, timeout: Optional[float] = None) -> Any:
+        '''
+        Raises queue.Empty exception if block is True and timeout 
+        '''
+        pass
+
+    def get_nowait(self) -> Any:
+        return self.get(False)
+    
+    @abstractmethod
+    def close(self) -> None:
+        pass
+
+    def join_thread(self) -> None:
+        pass
+
+    def cancel_join_thread(self) -> None:
+        pass
+
+    # extra methods  --------------------------------------------------------------
+
+    @abstractmethod
+    def initialize_receiver(self) -> None:
+        '''
+        function to execute in the receiver process
+        '''
+        pass
+
+    @abstractmethod
+    def initialize_sender(self) -> None:
+        '''
+        function to execute in the sender process
+        '''
+        pass
