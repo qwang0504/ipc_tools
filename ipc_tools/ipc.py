c73055c6052b9d17cd65ba83e98f5cc4dd9ec027
ElTinmar <martin.privat@hotmail.fr>
2023-11-08 15:51:15 +0100

    change class name

--- ipc_tools/ipc.py
+++ ipc_tools/ipc.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Optional, Any
 
-class IPC(ABC):
+class QueueLikeIPC(ABC):
     '''
     Multiprocessing Queue-like interface for various IPC methods
     '''
