8679b0834471575c1486a2970b69340ad5a40f24
ElTinmar <martin.privat@hotmail.fr>
2023-12-27 08:25:35 +0100

    update

--- ipc_tools/tests/test_priority_queue.py
+++ ipc_tools/tests/test_priority_queue.py
@@ -1,8 +1,12 @@
 from ipc_tools import PriorityQueue
 import numpy as np
 
+# test basic functionality
+
 SZ = (4,4)
-BIGARRAY = np.random.randint(0, 255, SZ, dtype=np.int32)
+ARRAY_0 = np.random.randint(0, 255, SZ, dtype=np.int32)
+ARRAY_1 = np.random.randint(0, 255, SZ, dtype=np.int32)
+ARRAY_2 = np.random.randint(0, 255, SZ, dtype=np.int32)
 
 Q = PriorityQueue(        
         num_items = 100, 
@@ -10,12 +14,15 @@ Q = PriorityQueue(
         data_type = np.int32
     )
 
-Q.put((1, BIGARRAY))
-Q.put((2, BIGARRAY))
-Q.put((3, BIGARRAY))
+Q.put((1, ARRAY_0))
+Q.put((10, ARRAY_1))
+Q.put((2, ARRAY_2))
 
 print(Q)
 
-Q.get()
-Q.get()
-Q.get()
+assert(np.allclose(Q.get(), ARRAY_1))
+assert(np.allclose(Q.get(), ARRAY_2))
+assert(np.allclose(Q.get(), ARRAY_0))
+
+# test multiprocessing
+
