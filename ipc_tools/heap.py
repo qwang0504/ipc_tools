51e3ab36baef292da717b0963e1ee54ad149d578
ElTinmar <martin.privat@hotmail.fr>
2024-01-11 17:20:47 +0100

    update

--- ipc_tools/heap.py
+++ ipc_tools/heap.py
@@ -574,36 +574,22 @@ class SharedMinMaxHeap(Heap):
     def __str__(self):
         return str(list(self.heap[:self.numel]))
 
-class SharedHeapTuple:
+class SharedMinHeapTuple:
     def __init__(
-            self, 
-            heapsize: int, 
-            tuplen: int = 1, 
-            sortkey: int = 0, 
-            heaptype: HeapType = HeapType.MINHEAP
+        self, 
+        heapsize: int, 
+        tuplen: int = 1, 
+        sortkey: int = 0, 
         ) -> None:
         
         if sortkey >= tuplen:
             raise ValueError('sortkey should be between 0 and tuplen-1')
-         
+            
         self.heapsize = heapsize
         self.tuplen = tuplen
         self.sortkey = sortkey
         self.numel = 0
         self.heap = RawArray('d', self.heapsize*self.tuplen)
-        self.heaptype = heaptype
-        if heaptype == HeapType.MINHEAP:
-            self.push = self._push_min
-            self.pop = self._pop_min
-            self.poppush = self._poppush_min
-            self.pushpop = self._pushpop_min
-        elif heaptype == HeapType.MAXHEAP:
-            self.push = self._push_max
-            self.pop =  self._pop_max
-            self.poppush = self._poppush_max
-            self.pushpop = self._pushpop_max
-        else:
-            raise ValueError('Unknown heap type')
 
     def get_heap_array(self) -> RawArray:
         return self.heap
@@ -614,21 +600,13 @@ class SharedHeapTuple:
     
     def clear(self):
         self.numel = 0
-    
-    def _push_min(self, item: Tuple) -> None:
-        """Push item onto heap, maintaining the heap invariant."""
+
+    def push(self, item: Tuple) -> None:
         self.heap[self.numel*self.tuplen:(self.numel+1)*self.tuplen] = item
         self._siftdown_min(0, self.numel*self.tuplen)
         self.numel += 1
 
-    def _push_max(self, item: Tuple) -> None:
-        """Push item onto heap, maintaining the heap invariant."""
-        self.heap[self.numel*self.tuplen:(self.numel+1)*self.tuplen] = item
-        self._siftdown_max(0, self.numel*self.tuplen)
-        self.numel += 1
-
-    def _pop_min(self) -> Tuple:
-        """Pop the smallest item off the heap, maintaining the heap invariant."""
+    def pop(self) -> Tuple:
         self.numel -= 1
         if self.numel < 0:
             self.numel = 0
@@ -637,57 +615,19 @@ class SharedHeapTuple:
         if self.numel > 0:
             returnitem = tuple(self.heap[0:self.tuplen])
             self.heap[0:self.tuplen] = lastelt
-            self._siftup_min(0)
-            return returnitem
-        return lastelt
-    
-    def _pop_max(self) -> Tuple:
-        """Maxheap version of a heappop."""
-        self.numel -= 1
-        if self.numel < 0:
-            self.numel = 0
-            raise IndexError('pop from empty heap') 
-        lastelt = tuple(self.heap[self.numel*self.tuplen:(self.numel+1)*self.tuplen])    
-        if self.numel > 0:
-            returnitem = tuple(self.heap[0:self.tuplen])
-            self.heap[0:self.tuplen] = lastelt
-            self._siftup_max(0)
+            self._siftup(0)
             return returnitem
         return lastelt
 
-    def _pushpop_min(self, item: Tuple) -> Tuple:
-        """Fast version of a heappush followed by a heappop."""
-        if self.numel > 0 and self.heap[0] < item[0]:
-            item, self.heap[0:self.tuplen] = self.heap[0:self.tuplen], item
-            self._siftup_min(0)
-        return item
-    
-    def _pushpop_max(self, item: Tuple) -> Tuple: # not sure that works, please test 
-        """Fast version of a heappush followed by a heappop."""
-        if self.numel > 0 and self.heap[0] < item[0]:
-            item, self.heap[0:self.tuplen] = self.heap[0:self.tuplen], item
-            self._siftup_max(0)
-        return item
-    
-    def _poppush_min(self, item: Tuple) -> Tuple:
-        """Pop and return the current smallest value, and add the new item."""
+    def replace(self, item: Tuple) -> Tuple:
         if self.numel == 0:
             raise IndexError('replace from empty heap') 
         returnitem = tuple(self.heap[0:self.tuplen])   
         self.heap[0:self.tuplen] = item
-        self._siftup_min(0)
-        return returnitem
-    
-    def _poppush_max(self, item: Tuple) -> Tuple:
-        """Maxheap version of a heappop followed by a heappush."""
-        if self.numel == 0:
-            raise IndexError('replace from empty heap') 
-        returnitem = self.heap[0:self.tuplen]    
-        self.heap[0:self.tuplen] = item
-        self._siftup_max(0)
+        self._siftup(0)
         return returnitem
 
-    def _siftdown_min(self, startpos: int, pos: int) -> None:
+    def _siftdown(self, startpos: int, pos: int) -> None:
         newitem = tuple(self.heap[pos:pos+self.tuplen])
         # Follow the path to the root, moving parents down until finding a place
         # newitem fits.
@@ -701,7 +641,85 @@ class SharedHeapTuple:
             break
         self.heap[pos:pos+self.tuplen] = newitem
 
-    def _siftdown_max(self, startpos: int, pos: int) -> None:
+    def _siftup(self, pos: int) -> None:
+        endpos = self.numel*self.tuplen
+        startpos = pos
+        newitem = tuple(self.heap[pos:pos+self.tuplen])
+        childpos = (2*pos//self.tuplen + 1)*self.tuplen    # leftmost child position
+
+        # Bubble up the smaller child until hitting a leaf.
+        while childpos < endpos:
+            # Set childpos to index of smaller child.
+            rightpos = (childpos//self.tuplen + 1) * self.tuplen
+            if rightpos < endpos and not self.heap[childpos] < self.heap[rightpos]:
+                childpos = rightpos
+            # Move the smaller child up.
+            self.heap[pos:pos+self.tuplen] = self.heap[childpos:childpos+self.tuplen]
+            pos = childpos
+            childpos = (2*pos//self.tuplen + 1)*self.tuplen
+
+        # The leaf at pos is empty now.  Put newitem there, and bubble it up
+        # to its final resting place (by sifting its parents down).
+        self.heap[pos:pos+self.tuplen] = newitem
+        self._siftdown(startpos, pos)
+
+    def __str__(self):
+        return str(list(self.heap[:self.numel*self.tuplen]))
+    
+class SharedMaxHeapTuple:
+    def __init__(
+        self, 
+        heapsize: int, 
+        tuplen: int = 1, 
+        sortkey: int = 0, 
+        ) -> None:
+        
+        if sortkey >= tuplen:
+            raise ValueError('sortkey should be between 0 and tuplen-1')
+            
+        self.heapsize = heapsize
+        self.tuplen = tuplen
+        self.sortkey = sortkey
+        self.numel = 0
+        self.heap = RawArray('d', self.heapsize*self.tuplen)
+
+    def get_heap_array(self) -> RawArray:
+        return self.heap
+    
+    def get_tuple_elements(self, key: int = 0) -> List:
+        selection = [self.heap[i*self.tuplen+key] for i in range(self.numel)]
+        return selection
+    
+    def clear(self):
+        self.numel = 0
+
+    def push(self, item: Tuple) -> None:
+        self.heap[self.numel*self.tuplen:(self.numel+1)*self.tuplen] = item
+        self._siftdown(0, self.numel*self.tuplen)
+        self.numel += 1
+
+    def pop(self) -> Tuple:
+        self.numel -= 1
+        if self.numel < 0:
+            self.numel = 0
+            raise IndexError('pop from empty heap') 
+        lastelt = tuple(self.heap[self.numel*self.tuplen:(self.numel+1)*self.tuplen])    
+        if self.numel > 0:
+            returnitem = tuple(self.heap[0:self.tuplen])
+            self.heap[0:self.tuplen] = lastelt
+            self._siftup(0)
+            return returnitem
+        return lastelt
+    
+    def replace(self, item: Tuple) -> Tuple:
+        if self.numel == 0:
+            raise IndexError('replace from empty heap') 
+        returnitem = self.heap[0:self.tuplen]    
+        self.heap[0:self.tuplen] = item
+        self._siftup(0)
+        return returnitem
+
+    def _siftdown(self, startpos: int, pos: int) -> None:
         'Maxheap variant of _siftdown_min'
         newitem = self.heap[pos:pos+self.tuplen]
         # Follow the path to the root, moving parents down until finding a place
@@ -716,33 +734,14 @@ class SharedHeapTuple:
             break
         self.heap[pos:pos+self.tuplen] = newitem
 
-    def _siftup_min(self, pos: int) -> None:
-        endpos = self.numel*self.tuplen
-        startpos = pos
-        newitem = tuple(self.heap[pos:pos+self.tuplen])
-        # Bubble up the smaller child until hitting a leaf.
-        childpos = (2*pos//self.tuplen + 1)*self.tuplen    # leftmost child position
-        while childpos < endpos:
-            # Set childpos to index of smaller child.
-            rightpos = (childpos//self.tuplen + 1) * self.tuplen
-            if rightpos < endpos and not self.heap[childpos] < self.heap[rightpos]:
-                childpos = rightpos
-            # Move the smaller child up.
-            self.heap[pos:pos+self.tuplen] = self.heap[childpos:childpos+self.tuplen]
-            pos = childpos
-            childpos = (2*pos//self.tuplen + 1)*self.tuplen
-        # The leaf at pos is empty now.  Put newitem there, and bubble it up
-        # to its final resting place (by sifting its parents down).
-        self.heap[pos:pos+self.tuplen] = newitem
-        self._siftdown_min(startpos, pos)
-
-    def _siftup_max(self, pos):
+    def _siftup(self, pos):
         'Maxheap variant of _siftup_min'
         endpos = self.numel*self.tuplen
         startpos = pos
         newitem = tuple(self.heap[pos:pos+self.tuplen])
-        # Bubble up the larger child until hitting a leaf.
         childpos = (2*pos//self.tuplen + 1)*self.tuplen   # leftmost child position
+
+        # Bubble up the larger child until hitting a leaf.
         while childpos < endpos:
             # Set childpos to index of larger child.
             rightpos = (childpos//self.tuplen + 1) * self.tuplen
@@ -752,10 +751,161 @@ class SharedHeapTuple:
             self.heap[pos:pos+self.tuplen] = self.heap[childpos:childpos+self.tuplen]
             pos = childpos
             childpos = (2*pos//self.tuplen + 1)*self.tuplen
+
         # The leaf at pos is empty now.  Put newitem there, and bubble it up
         # to its final resting place (by sifting its parents down).
         self.heap[pos:pos+self.tuplen] = newitem
-        self._siftdown_max(startpos, pos)
+        self._siftdown(startpos, pos)
 
     def __str__(self):
         return str(list(self.heap[:self.numel*self.tuplen]))
+    
+class SharedMinMaxHeapTuple(Heap):
+    def __init__(
+        self, 
+        heapsize: int, 
+        tuplen: int = 1, 
+        sortkey: int = 0, 
+        ) -> None:
+        
+        if sortkey >= tuplen:
+            raise ValueError('sortkey should be between 0 and tuplen-1')
+            
+        self.heapsize = heapsize
+        self.tuplen = tuplen
+        self.sortkey = sortkey
+        self.numel = 0
+        self.heap = RawArray('d', self.heapsize*self.tuplen)
+
+    def get_heap_array(self) -> RawArray:
+        return self.heap
+    
+    def get_tuple_elements(self, key: int = 0) -> List:
+        selection = [self.heap[i*self.tuplen+key] for i in range(self.numel)]
+        return selection
+    
+    def clear(self):
+        self.numel = 0
+
+    def push(self, item):
+        self.heap[self.numel] = item
+        self._siftup(self.numel)
+        self.numel += 1
+        
+    def pop_min(self):
+        self.numel -= 1
+        if self.numel < 0:
+            self.numel = 0
+            raise IndexError('pop from empty heap') 
+      
+        returnitem = self.heap[0]
+        lastelt = self.heap[self.numel]   
+        if self.numel > 0:
+            self.heap[0] = lastelt
+            self._siftdown(0)
+        return returnitem
+
+    def pop_max(self):
+        if self.numel < 2:
+            return self.pop_min()
+        
+        self.numel -= 1
+        returnitem = self.heap[1]
+        lastelt = self.heap[self.numel]    
+        if self.numel > 2:
+            self.heap[1] = lastelt
+            self._siftdown(1)
+        return returnitem
+
+    def _siftup(self, pos):
+        if is_even(pos):
+            sibling = pos - 1 if (pos & 3) == 0 else pos >> 1
+            if sibling > 0 and self.heap[sibling] < self.heap[pos]:
+                self.heap[sibling], self.heap[pos] = self.heap[pos], self.heap[sibling]
+                self._siftup_max(sibling)
+            else:
+                self._siftup_min(pos)
+        else:
+            if self.heap[pos-1] > self.heap[pos]:
+                self.heap[pos-1], self.heap[pos] = self.heap[pos], self.heap[pos-1]
+                self._siftup_min(pos-1)
+            else:
+                self._siftup_max(pos)
+
+    def _siftup_min(self, pos):
+        val = self.heap[pos]
+        while pos > 0:
+            parent = (pos >> 1) - 2 if (pos & 3) == 0 else (pos >> 1) - 1
+            if self.heap[parent] <= val:
+                break
+            self.heap[pos] = self.heap[parent]
+            pos = parent
+        self.heap[pos] = val
+
+    def _siftup_max(self, pos):
+        val = self.heap[pos]
+        while pos > 1:
+            parent = pos >> 1 if (pos & 3) == 3 else (pos >> 1) - 1
+            if self.heap[parent] >= val:
+                break
+            self.heap[pos] = self.heap[parent]
+            pos = parent
+        self.heap[pos] = val
+
+    def _siftdown(self, pos):
+        if is_even(pos):
+            self._siftdown_min(pos)
+        else:
+            self._siftdown_max(pos)
+
+    def _siftdown_min(self, pos):
+        if pos + 1 < self.numel and self.heap[pos] > self.heap[pos+1]:
+            self.heap[pos], self.heap[pos+1] = self.heap[pos+1], self.heap[pos]
+
+        candidate, end = (pos + 1) << 1, min(self.numel, (pos + 3) << 1)
+        if candidate >= self.numel:
+            return
+
+        for i in range(candidate+1, end):
+            if self.heap[i] < self.heap[candidate]:
+                candidate = i
+
+        if self.heap[candidate] < self.heap[pos]:
+            self.heap[pos], self.heap[candidate] = self.heap[candidate], self.heap[pos]
+            self._siftdown(candidate)
+
+    def _siftdown_max(self, pos):
+        if self.heap[pos-1] > self.heap[pos]:
+            self.heap[pos-1], self.heap[pos] = self.heap[pos], self.heap[pos-1]
+
+        candidate, end = pos << 1, min(self.numel, (pos + 2) << 1)
+        if candidate >= self.numel:
+            return
+        
+        for i in range(candidate+1, end):
+            if self.heap[i] > self.heap[candidate]:
+                candidate = i
+
+        if self.heap[candidate] > self.heap[pos]:
+            self.heap[pos], self.heap[candidate] = self.heap[candidate], self.heap[pos]
+            self._siftdown(candidate)
+
+    def replace_min(self, item):
+        val = self.heap[0]
+        self.heap[0] = item
+        self._siftdown(0)
+        return val
+
+    def replace_max(self, item):
+        if self.numel < 2:
+            return self.replace_min(item)
+        val = self.heap[1]
+        self.heap[1] = item
+        self._siftdown(1)
+        return val
+
+    def clear(self):
+        self.numel = 0
+
+    def __str__(self):
+        return str(list(self.heap[:self.numel]))
