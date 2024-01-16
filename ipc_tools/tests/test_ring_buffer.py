ae9f6b5fdff2b3d71124aa3d971401a91f24fe4a
Eltinmar <martin.privat@hotmail.fr>
2024-01-16 11:14:00 +0100

    update

--- ipc_tools/tests/test_ring_buffer.py
+++ ipc_tools/tests/test_ring_buffer.py
@@ -18,7 +18,7 @@ class Tests(unittest.TestCase):
         res = buf.get()
         self.assertTrue(np.allclose(res, ARRAY))
 
-    def test_structured_array(self):
+    def test_structured_array_0(self):
         dt = np.dtype([
             ('timestamp', np.float64, (1,)), 
             ('image', np.float32, (1024,1024))
@@ -33,6 +33,31 @@ class Tests(unittest.TestCase):
         res = buf.get()
         self.assertEqual(TS, res['timestamp'])
         self.assertTrue(np.allclose(res['image'], x['image']))
+
+    
+    def test_structured_array_1(self):
+        dt = np.dtype([
+            ('timestamp', np.float64, (1,)), 
+            ('image', np.float32, (1024,1024))
+        ])
+
+        Array_0 = np.random.uniform(low=-1, high=1, size=SZ).astype(np.float32)
+        Array_1 = np.random.uniform(low=-1, high=1, size=SZ).astype(np.float32)
+
+        x0 = np.array([(TS, Array_0)], dtype=dt)
+        x1 = np.array([(TS, Array_1)], dtype=dt)
+        buf = RingBuffer(
+            num_items=100,
+            item_shape=(1,),
+            data_type=dt
+        )
+        buf.put(x0)
+        buf.put(x1)
+        res0 = buf.get()
+        res1 = buf.get()
+
+        self.assertTrue(np.allclose(res0['image'], Array_0))
+        self.assertTrue(np.allclose(res1['image'], Array_1))
 
 if __name__ == '__main__':
     unittest.main()
