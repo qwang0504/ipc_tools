8b3fb932a03452bf3c6b0410e7e3dcdfead35cd7
ElTinmar <martin.privat@hotmail.fr>
2024-07-15 14:33:45 +0200

    rollback

--- ipc_tools/queue_like.py
+++ ipc_tools/queue_like.py
@@ -61,7 +61,6 @@ class QueueMP(Queue, QueueLike):
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs, ctx = get_context())
-        self.cancel_join_thread()
 
     def clear(self) -> None:
         try:
