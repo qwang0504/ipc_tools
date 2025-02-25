b7a3680ad7fd6ea285f8c0a0cc29f1f938e4beb5
qwang0504 <qing.wang@bi.mpg.de>
2025-02-25 19:09:12 +0100

    tutorial

--- ipc_tools/multiprocessing_tutorial.py
+++ ipc_tools/multiprocessing_tutorial.py
@@ -3,7 +3,8 @@ import multiprocessing
 import time
 import random
 import os
-from multiprocessing import Process, Lock, RLock, Value, Array
+from multiprocessing import Process, Lock, RLock, Value, Array, Event, Queue, Pipe
+from queue import Empty
 
 # All code derived from https://superfastpython.com/multiprocessing-in-python/ 
 
@@ -13,7 +14,6 @@ from multiprocessing import Process, Lock, RLock, Value, Array
 #     print('Process B before sleeping')
 #     time.sleep(t)
 #     print('Process B finished sleeping')
-
 
 # if __name__ == '__main__':
 #     process = Process(target=some_function, args=(10,)) #note that args expects a tuple
@@ -121,40 +121,89 @@ from multiprocessing import Process, Lock, RLock, Value, Array
 
 ### Reentrant locks 
 
-def child_process_task(lock, identifier, sleep_time):
-    with lock: 
-        process_id = os.getpid()
-        print(process_id)
-        print(f'Process {identifier} acquired lock, sleeping for {sleep_time}')
-        time.sleep(sleep_time)
-        report(lock, identifier) #RLock enables the same process to acquire the lock again
+# def child_process_task(lock, identifier, sleep_time):
+#     with lock: 
+#         process_id = os.getpid()
+#         print(process_id)
+#         print(f'Process {identifier} acquired lock, sleeping for {sleep_time}')
+#         time.sleep(sleep_time)
+#         report(lock, identifier) #RLock enables the same process to acquire the lock again
 
-def report(lock, identifier):
-    with lock: 
-        process_id = os.getpid()
-        print(process_id)
-        print(f'Process {identifier, process_id} finished sleeping')
+# def report(lock, identifier):
+#     with lock: 
+#         process_id = os.getpid()
+#         print(process_id)
+#         print(f'Process {identifier, process_id} finished sleeping')
 
-if __name__ == '__main__':
-    n_processes = 10
-    lock = RLock()
-    processes = [Process(target=child_process_task, args=(lock, i, random.random())) for i in range(n_processes)]
+# if __name__ == '__main__':
+#     n_processes = 10
+#     lock = RLock()
+#     processes = [Process(target=child_process_task, args=(lock, i, random.random())) for i in range(n_processes)]
 
-    for process in processes:
-        process.start()  #starts processes in order of i=0 to 10
-        print(f'Started process with PID {process.pid}') #prints each unique PID for processes arbitrarily assigned to 0-10
+#     for process in processes:
+#         process.start()  #starts processes in order of i=0 to 10
+#         print(f'Started process with PID {process.pid}') #prints each unique PID for processes arbitrarily assigned to 0-10
 
-    for process in processes:
-        process.join()
+#     for process in processes:
+#         process.join()
 
 
 
 ### Events - process-safe boolean flags 
 
+# def event_task(event, number):
+#     print(f'Process {number} waiting')
+#     event.wait()
+#     value = random.random()
+#     time.sleep(value)
+#     print(f'Process {number} got {value}')
 
-
+# if __name__ == '__main__':
+#     n_processes = 10
+#     event = Event()
+#     processes = [Process(target=event_task, args=(event, i)) for i in range(n_processes)]
+#     for process in processes:
+#         process.start()
+#     time.sleep(1)
+#     event.set()
+#     for process in processes:
+#         process.join()
+    
 
 
 ### Pipes and Queues
 
+def consumer(queue, t_refresh):
+    print('Consumer running...')
+    while True:
+        try:
+            index, item = queue.get(block=False)
+        except Empty:
+            print('Queue is empty, waiting for a while')
+            time.sleep(t_refresh)
+            continue
+        if item is None: 
+            break 
+        print(f'Consumer got item number {index}: {item}')
+    print('Consumer finished')
+    
 
+def producer(queue):
+    for i in range(10):
+        value = random.random()
+        time.sleep(value)
+        queue.put((i, value))
+    queue.put((i+1, None))
+    print('Producer finished')
+
+
+if __name__ == '__main__':
+    queue = Queue()
+    consumer_process = Process(target=consumer, args=(queue, 0.1))
+    producer_process = Process(target=producer, args=(queue,))
+
+    consumer_process.start()
+    producer_process.start()
+
+    consumer_process.join()
+    producer_process.join()
